0282311ea9da6856b5f9220b0ebe5c3583157e33
vipgao <928363128@qq.com>
2024-10-19 08:30:15 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.fubang.tech"];
-pub const PUBLIC_RS_PUB_KEY: &str = "s1CgI5he3AsO5FYJqimrks3i6GfCaYPfCIjQXxSfxwk=";
+pub const PUBLIC_RS_PUB_KEY: &str = "rHeFGZKWzVJMW9hNQVXHsJHr+xlW9chQTQDw29Q98eY=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
